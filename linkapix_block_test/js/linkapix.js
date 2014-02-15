567ac7c9b29f83dee61186b4e4bcda8eaca52d96
Thomas <thomas@tuomas.co.uk>
2014-02-15 14:55:34 +0000

    made concept way smarter
    you can now give up on a link and then click the endpoint to remove your
    progress and try again, you can no longer pass through another link, you
    can no longer go further than the number of the link you started with,
    can no longer join with self

--- linkapix_block_test/js/linkapix.js
+++ linkapix_block_test/js/linkapix.js
@@ -1,5 +1,9 @@
 var picking = false;
+var start = [0,0];
+var startblock = null;
 var number = 0;
+var previous = null;
+var links = [];
 
 function destroy_puzzle() {
     $(".linkapix").html("");
@@ -17,7 +21,7 @@ function json_to_puzzle(json_url) {
             });
         });
     });
-    var str = '[[{"number":"2","color":{"r":0,"g":0,"b":0}},{"number":"","color":{"r":0,"g":0,"b":0}}],[{"number":"1","color":{"r":0,"g":0,"b":0}},{"number":"","color":{"r":0,"g":0,"b":0}}]] ';
+    var str = '[[{"number":3,"color":{"r":0,"g":0,"b":0}},null,{"number":3,"color":{"r":0,"g":0,"b":0}},null,null],[null,null,null,{"number":1,"color":{"r":0,"g":0,"b":0}},null],[null,null,{"number":5,"color":{"r":0,"g":0,"b":0}},{"number":2,"color":{"r":0,"g":0,"b":0}},null],[null,null,null,{"number":2,"color":{"r":0,"g":0,"b":0}},null],[{"number":5,"color":{"r":0,"g":0,"b":0}},null,null,null,null]]';
     return JSON.parse(str);
 };
 
@@ -31,11 +35,13 @@ function build_puzzle(puzzle) {
         var blocks = [];
         $.each(row, function(cid, block) {
             number = block !== null ? block.number : "";
-            blocks.push("<td>" + number + "</td>");
+            console.log('x');
+            blocks.push('<td data-number="'+number+'" data-x="'+cid+'" data-y="'+rid+'">' + number + "</td>");
         });
         tr.append(blocks);
         table.append(tr)
     });
+    //state = new State();
     register_game_events();
 
 };
@@ -49,34 +55,106 @@ $("#launch_test").click(function() {
 
 function register_game_events() {
     $("td").mousedown(function() {
-        if ($(this).is(':empty')) {
+        if ($(this).hasClass('link-end')) {
+            $(".linkapix").trigger('editlink', [$(this)]);
+        }
+        if($(this).hasClass('linked') || $(this).hasClass('unfinished')) {
             return;
-        }
-        else if ($(this).html() == '1') {
-            $(this).css('background-color', '#222');
-        }
-        else {
-            console.log($(this).html());
-            number = $(this).html();
-            picking = true;
-            $(this).css('background-color', '#999').addClass('picking');
+        } else {
+            $(".linkapix").trigger('startlink', [$(this), $(this).data('x'), $(this).data('y')]);
         }
     });
 
     $("td").mouseover(function() {
         if (picking) {
-            $(this).css('background-color', '#999').addClass('picking');
+            $(".linkapix").trigger('extendlink', [$(this)]);
         }
     });
 
     $("td").mouseup(function() {
         if (picking) {
-            if ($(this).html() == number && $("td.picking").length == number) {
-                $("td.picking").css('background-color', '#222').removeClass('picking');
-                $(this).css('background-color', '#222');
+            $('.linkapix').trigger('stoplink', [$(this)]);
+        }
+    });
+
+    $('.linkapix').on('startlink', function(event, block, x, y) {
+        start = [x, y];
+        startblock = block;
+
+        if (block.data('number') == '') {
+            return;
+        }
+        else if (block.data('number') == '1') {
+            block.addClass('linked');
+        }
+        else {
+            //console.log($(this).html());
+            number = block.data('number');
+            picking = true;
+            block.addClass('picking');
+            links.push(block);
+        }
+    });
+
+    $('.linkapix').on('extendlink', function(event, block) {
+        if (previous !== null) previous.html('');
+        if (block.hasClass('unfinished') || block.hasClass('linked') || block.hasClass('picking') || $('td.picking').length >= number) {
+            picking = false;
+            if ([block.data('x'), block.data('y')] != start) {
+                //console.log(block.data('x'), block.data('y'));
+                //console.log(previous.data('x'), previous.data('y'));
+                if(previous == null) {
+                    picking = false;
+                    $("td.picking").removeClass('picking');
+                    links = [];
+                    return
+                }
+                $(this).trigger('stoplink', [previous]);
+            }
+            $("td.picking").removeClass('picking');
+            return;
+        } else {
+            block.addClass('picking');
+            links.push(block);
+            block.html($('td.picking').length);
+            console.log(block != startblock);
+            console.log([block.data('x'), block.data('y')] != start)
+            if (block != startblock) {
+                previous = block;
+            }
+        }
+    });
+
+    $('.linkapix').on('stoplink', function(event, block) {
+            //console_log($("td.picking").length);
+            previous = null;
+            if (block.data('number') == '') {
+                len = $("td.picking").length;
+                block.addClass('unfinished link-end');
+                block.html(len);
+                block.data('progress', $("td.picking").length);
+                links.push(block);
+                block.data('links', links);
+                links = [];
+                $("td.picking").removeClass('picking').addClass('unfinished');
+            } else if (block.data('number') == number && $("td.picking").length == number) {
+                $("td.picking").removeClass('picking');
+                block.addClass('linked');
+                $.each(links, function(key, val) {
+                    val.addClass('linked');
+                });
             }
             $("td.picking").css('background-color', '#fff').removeClass('picking');
             picking = false;
-        }
+    });
+
+    $('.linkapix').on('editlink', function(event, block) {
+        block.html('');
+        block.removeClass('link-end');
+        $.each(block.data('links'), function(key, val) {
+            val.removeClass('unfinished');
+        });
     });
 };
+
+$("#launch_test").trigger('click');
